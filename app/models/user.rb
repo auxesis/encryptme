26b046a7f2fb4cdc5dfb2f16951dbc6c41065b40
Lindsay Holmwood <lindsay@holmwood.id.au>
2022-08-07 23:53:07 +1000

    Add users, and encrypt them

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  include ActiveStash::Search
+  stash_index :name, :email, :dob
+  encrypts :name, :email, :dob
 end
--- db/migrate/20220513062236_create_users.rb
+++ db/migrate/20220513062236_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :name
       t.string :email
       t.string :password_digest
-      t.date :dob
+      t.string :dob
 
       t.timestamps
     end
